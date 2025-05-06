cad0c2fe8944a9d5aea036225646d5bca76d7f43
Anton <an-78@yandex.kz>
2025-05-07 01:55:42 +0300

    time_control_01

---
+++ sprint2/problems/time_control/solution/src/logging_request_handler.cpp
@@ -0,0 +1,16 @@
+#include "logging_request_handler.h"
+
+
+namespace log_handler{
+
+// void LoggingRequestHandler::LogRequest(const Request& r){
+
+// }
+// void LoggingRequestHandler::LogResponse(const Response& r){
+
+// }
+
+
+
+
+}// namespace logging_request_handler
